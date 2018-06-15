6219e27c6e6eba341d16c7b499fa5bdfe562658f
akoszczka <blanter50@gmail.com>
2018-06-15 21:28:04 +0200

    Dodanie funkcji pokazującej ilość żywych komórek

--- main.c
+++ main.c
@@ -21,19 +21,56 @@ void rysuj(char plansza[rozmiar_y][rozmiar_x])
         printf("\n");
     }
 }
+
+void licz_komorki(char plansza[rozmiar_y][rozmiar_x])
+{
+    int i,j;
+    int komorki = 0;
+    for(i=0; i<rozmiar_y; i++)
+    {
+        for(j=0; j<rozmiar_x; j++)
+        {
+            if((plansza[i][j])=='X')
+            komorki++;
+        }
+    }
+    printf("Liczba zywych komorek: %d \n", komorki);
+}
 void pisz(char plansza[rozmiar_y][rozmiar_x])
 {
-  plansza[rozmiar_y/2][rozmiar_x/2]='X';
-  plansza[(rozmiar_y/2)][(rozmiar_x/2)+1]='X';
-  plansza[(rozmiar_y/2)][(rozmiar_x/2)-1]='X';
+   /* int i, j;
+    char wybierz;
+    FILE *dane;
+    dane = fopen("dane1.txt", "r");
 
+    switch(wybierz)
+    {
+        case '1':
+            for(i=0; i<rozmiar_y; i++)
+                for(j=0; j<rozmiar_x; j++)
+                {
+                        plansza[i][j]=getc(dane);
+                        if(j==0 && plansza[i][j]==10) plansza[i][j]=getc(dane);
+                        if(plansza[i][j]==10)
+                        {
+                        plansza[i][j]=0;
+                        }
+                }
+
+        break;
+
+      case '2':
+      case '3':
+      case '4':*/
   plansza[0][1]='X';
   plansza[1][2]='X';
   plansza[2][0]='X';
   plansza[2][1]='X';
   plansza[2][2]='X';
+  int i;
+    for(i=0;i<rozmiar_x;i++)
+        plansza[rozmiar_y/2][i]='X';
 }
-
 void plansza_krok(char plansza_a[rozmiar_y][rozmiar_x], char plansza_b[rozmiar_y][rozmiar_x]){
     int i,j;
 
@@ -43,8 +80,9 @@ void plansza_krok(char plansza_a[rozmiar_y][rozmiar_x], char plansza_b[rozmiar_y
         }
 }
 
-void krok(char plansza_a[rozmiar_y][rozmiar_x], char plansza_b[rozmiar_y][rozmiar_x]){
-
+void krok(char plansza_a[rozmiar_y][rozmiar_x], char plansza_b[rozmiar_y][rozmiar_x], int *martwe)
+{
+    *martwe=0;
     int i,j;
     int ls=0;
 
@@ -66,11 +104,16 @@ void krok(char plansza_a[rozmiar_y][rozmiar_x], char plansza_b[rozmiar_y][rozmia
                     plansza_b[i][j]='X';
 
             else if(ls<2 || ls>3)
-                    plansza_b[i][j]=' ';
-
+            {
+            martwe++;
+            plansza_b[i][j]=' ';
+            }
             ls=0;
         }
+
+
     }
+
     plansza_krok(plansza_a,plansza_b);
 }
 
@@ -79,22 +122,28 @@ void krok(char plansza_a[rozmiar_y][rozmiar_x], char plansza_b[rozmiar_y][rozmia
 
 int main()
 {
+    int martwe=0;
     char plansza[rozmiar_y][rozmiar_x];
     char plansza_a[rozmiar_y][rozmiar_x];
     char plansza_b[rozmiar_y][rozmiar_x];
+    char wybierz;
     int generacja=0;
     char koniec;
     wyczysc(plansza_a);
     wyczysc(plansza_b);
     rysuj(plansza);
+   /* printf("Podaj wybor: ");
+    scanf("%d", &wybierz);*/
     pisz(plansza_a);
     pisz(plansza_b);
     do{
     system("cls");
     rysuj(plansza_b);
+    licz_komorki(plansza_b);
     printf("Generacja numer: %d\n", ++generacja);
+    printf("Liczba komorek ktore umarly od startu programu: %d\n", martwe);
     scanf("%c", &koniec);
-    krok(plansza_a,plansza_b);
+    krok(plansza_a,plansza_b,&martwe);
     }while(koniec != 'q');
     return 0;
 }
